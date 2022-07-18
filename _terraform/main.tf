80bee9aaa28b61c8ff339a9078ff37b6dcc67910
Your Name <you@example.com>
2022-07-19 00:02:10 +0200

    Added Terraform template + workflow for deploying terraform infrastructure and application code

---
+++ _terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sd4cc-terraform-backend"
+    key    = "main/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "sd4cc-terraform-backend" {
+  bucket = "sd4cc-terraform-backend"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
